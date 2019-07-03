171002e855830956a10a831be1c36efab43bb85c
Bright Robert <guruskid@yahoo.com>
2019-07-03 20:08:13 +0100

    School Manager

--- application/views/layout/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="pull-right">
-        ©2014 Global School Management Stystem | Developed by <a target="_blank" href="https://codecanyon.net/user/codetroopers">Codetroopers</a>
-    </div>
-    <div class="clearfix"></div>
-</footer>
